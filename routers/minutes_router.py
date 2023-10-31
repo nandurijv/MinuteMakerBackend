dc12ff8f23cb47a1035bbfeb41347e0f81530ed3
Shreyaanp <shreyaan.pradhan2020@vitstudent.ac.in>
2023-11-01 01:28:07 +0530

    fix internal server error -1

--- routers/minutes_router.py
+++ routers/minutes_router.py
@@ -2,6 +2,8 @@ from middlewares.auth_controller import auth_model
 from app import app, cross_origin
 from controllers.minutes_controller import minutes_controller
 from flask import request
+import  requests
+import json
 
 obj = minutes_controller()
 authenticator=auth_model()
@@ -48,3 +50,28 @@ def deleteminutes():
 @authenticator.token_auth
 def updateminutes():
     return obj.updateminute(request.json)
+
+@app.route("/minutes/summary", methods=["POST"])
+@cross_origin()
+@authenticator.token_auth
+def getsummary():
+    try:
+        endpoint = "https://1b04-34-170-240-137.ngrok.io" # Replace with your endpoint
+        usermessage = request.json["transcript"][:100]
+        response_final = "write me a Summary abstract, keypoints and conclusion of the meeting thatt starts with the following" + usermessage+"..."+ "\n"+"in the following way : " + "1. Abstract" + "\n" + "2. Key Points" + "\n" + "3. Conclusion"
+        payload = {
+            "body": json.dumps({
+                "messageResponse": response_final
+            })
+        }
+
+        response = requests.post(f"{endpoint}/chatbot", json=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to send POST request. HTTP Status Code: {response.status_code}")
+            return {"message": f"Failed to send POST request. HTTP Status Code: {response.status_code}"}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {"message": f"An error occurred: {e}"}
